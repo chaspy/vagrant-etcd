04eb36e19c851114d6fa497fc376d07bf2e3526a
kondo takeshi <chaspy@users.noreply.github.com>
2019-05-13 03:01:32 +0900

    Fix file name for environment variable

--- init.sh
+++ init.sh
@@ -30,8 +30,8 @@ sudo chown -R etcd:etcd /var/lib/etcd/
 # Set environment variable for etcd cluster
 export ETCD_HOST_IP=$(ip addr show enp0s8 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 export ETCD_NAME=$(hostname -s)
-echo "ETCD_HOST_IP=${ETCD_HOST_IP}" >> /etc/default/environment_variable
-echo "ETCD_NAME=${ETCD_NAME}" >> /etc/default/environment_variable
+echo "ETCD_HOST_IP=${ETCD_HOST_IP}" >> /etc/default/etcd
+echo "ETCD_NAME=${ETCD_NAME}" >> /etc/default/etcd
 
 # Configure Systemd and start etcd service
 # Create a new systemd service file for etcd.
